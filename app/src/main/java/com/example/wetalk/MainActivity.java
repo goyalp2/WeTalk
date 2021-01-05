305d06ff6d3449d2b623ec3ec91bb3451d06edb0
goyalp2 <vibhug7@gmail.com>
2021-01-05 21:05:15 +0530

    Second Commit

--- app/src/main/java/com/example/wetalk/MainActivity.java
+++ app/src/main/java/com/example/wetalk/MainActivity.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.*;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     
-    @BindView(R.id.main_page_toolbar)
-    Toolbar mToolbar;
+    @BindView(R.id.main_page_toolbar)    Toolbar mToolbar;
+    @BindView(R.id.main_tabs)    TabLayout mTabLayout;
+    @BindView(R.id.main_tabs_pager)    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
